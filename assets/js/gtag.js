08d4d0301c5ea349222821f8335a0264df34315b
DevTechSoft5_ <devtechsoft5@gmail.com>
2026-03-09 19:15:35 +0530

    all

---
+++ assets/js/gtag.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+/* Google Analytics measurement id */
+gtag('config', 'G-MGTDGLQPSH');
